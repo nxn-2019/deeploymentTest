801a5bce54a78af41b21f9b6ade9ba186b7525aa
nxn-2019 <57587281+nxn-2019@users.noreply.github.com>
2021-02-23 18:14:46 +0400

    Redeploy

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
 DOCKER_NAME=district-deployment-test
+CONTAINER_IDS=$(docker container ls -a -q -f name=${DOCKER_NAME})
 
-docker pull nxn-2019/deployment-test:latest
-docker stop ${DOCKER_NAME}
+if [[ "$CONTAINER_IDS" != "" ]]; then
+    docker container rm -f ${CONTAINER_IDS} || true
+    exit
+fi
+
+docker login --username=nxn2021 --password=NXNLogin@2019
+
+docker pull nxn2021/deployment-test:latest
 docker system prune -f
 #docker run -d --name=${DOCKER_NAME} nxn-2019/deployment-test:latest
 docker run --name ${DOCKER_NAME} -p 2331:80 ${1} -d ${DOCKER_NAME} nxn-2019/deployment-test:latest
+
+docker container ls -a
